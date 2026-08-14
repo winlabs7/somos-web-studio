d83787e4379d766152f4cd490c3f2c7d3e73ff25
winlabs7 <edgar@sym.team>
2026-08-14 06:59:11 -0500

    Hiring page: five seats + equipo voice (site.js)

--- casacreadores/web/site.js
+++ casacreadores/web/site.js
@@ -86,6 +86,7 @@
     if (modalClose) modalClose.addEventListener("click", closeModal);
     overlay.addEventListener("click", function (e) {
       if (e.target === overlay) closeModal();
+      if (e.target.closest('a[href="#aplicar"]')) closeModal();
     });
   }
 
